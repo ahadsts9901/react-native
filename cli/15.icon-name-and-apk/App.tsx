3e9653a31817496852cc9e7b0b95da1a9057f9f1
ahadsts9901 <ahadsts990@gmail.com>
2024-10-14 14:50:34 +0500

    apk

---
+++ cli/15.icon-name-and-apk/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#464646", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ color: "#fff", fontSize: 32 }}>React Native Build</Text>
+    </View>
+  )
+}
+
+export default App
